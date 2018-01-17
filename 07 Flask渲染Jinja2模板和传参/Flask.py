9ffcfd207b4a67677ad4fc1a12c3356dbcc488e7
YummyELine <30431180+YummyELine@users.noreply.github.com>
2018-01-17 15:21:48 +0800

    Update Flask.py

--- 07 Flask渲染Jinja2模板和传参/Flask.py
+++ 07 Flask渲染Jinja2模板和传参/Flask.py
@@ -6,9 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    class Person(object):
+        name = '黄'
+        age = 20
+    p = Person()
     context = {'username': '小E',
               'gender': '男',
-              'age': '18'}
+              'age': 18,
+               'person': p,
+               'websites':{'baidu':'www.baidu.com',
+                           'google':'www.google.com'
+                           }
+               }
     return  render_template('index.html', **context)
     # 参数少可用这个方式 return render_template('index.html', username='小E', gender='男', age='18')
 
