d34d9f51524f6f1712507ac1890771b43c18050f
qingyu li <liqy79@qq.com>
2018-01-30 08:58:43 +0800

    ud

---
+++ 13 数据库相关/11 分开‘models’已经解决循环引用/models.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding:utf-8
+from exts import db
+
+
+class Artice(db.Model):
+    __tablename__ = 'article'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
---
+++ 13 数据库相关/11 分开‘models’已经解决循环引用/models_sep.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+from flask import Flask
+from exts import db
+from models import Artice
+import config
+
+app = Flask(__name__)
+app.config.from_object(config)
+db.init_app(app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
